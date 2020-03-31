999dfffdfa215facf66035d2e4b10751d61c2071
SupaFresh <31774924+SupaFresh@users.noreply.github.com>
2020-03-30 18:28:42 -0700

    Basic test
    test while loop

---
+++ Anti Adverb 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Anti_Adverb_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Please input a word. Lowercase only!");
+                string word = Console.ReadLine();
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
